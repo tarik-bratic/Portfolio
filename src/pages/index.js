b05772a40a18eb9f0297dc81264ae0c5ee5c22c2
Tariq <tarik.bratic@gmx.com>
2024-10-04 15:58:32 +0200

    Website release

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,146 @@
-import * as React from "react"
+import React, { useState, useEffect, useRef } from "react";
+import Typed from "typed.js";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import ProfilePic from "../images/myself.jpeg";
+import UniLogo from "../images/kth.png";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+import * as appStyles from "../styles/app.module.css"
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+const App = () => {
+  // Create reference to store the DOM element containing the animation
+  const statusContent = useRef(null);
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+  useEffect(() => {
+    const typed = new Typed(statusContent.current, {
+      strings: ['B.Sc. Comp. Eng.', 'Full-stack Developer'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      showCursor: false
+    });
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+  const toggleMenu = () => { 
+    setMenuOpen(!menuOpen); 
+  }
 
-const IndexPage = () => {
+  const scrollIntoSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <>
+      <header>
+        {/* Navigation bar, compatible for mobile and desktop */}
+        <nav className={`${appStyles.navbar} ${!menuOpen ? appStyles.menuClosed : ''}`}>
+          {/* Only displayed on mobile */}
+          <h1 className={appStyles.navTitle}>Tarik Bratic</h1>
+          <svg onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 40 50">
+            <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
+          </svg>
+          {/* Displayed both devices */}
+          <ul className={`${appStyles.menu} ${menuOpen ? appStyles.menuOpen : ''}`}>
+            <li className={appStyles.menuItem} onClick={() => scrollIntoSection('home')}>Home</li>
+            <li className={appStyles.menuItem} onClick={() => scrollIntoSection('about')}>About Me</li>
+            <li className={appStyles.menuItem} onClick={() => scrollIntoSection('career')}>Career</li>
+          </ul>
+        </nav>
+        <section id="home" className={appStyles.heroSection}>
+          <section className={appStyles.content}>
+            {/* Text window. My name and status. */}
+            <div className={appStyles.intro}>
+              <span className={appStyles.greeting}>Hello, I'm Tarik</span>
+              <span className={appStyles.status} ref={statusContent}></span>
+            </div>
+            {/* Buttons CV and Connect */}
+            <div className={appStyles.actions}>
+              <button className={appStyles.button} type="button">Resume</button>
+              <button className={appStyles.button} type="button">Connect</button>
+            </div>
+          </section>
+          {/* Img of myself and uni logo */}
+          <div className={appStyles.images}>
+            <img className={appStyles.profileImage} src={ProfilePic} alt="Profile picture of Tarik" />
+            <img className={appStyles.universityLog} src={UniLogo} alt="University Logo" />
+          </div>
+        </section>
+      </header>
+      <main>
+        <section id="about" className={appStyles.about}>
+          <h2 className={appStyles.aboutTitle}>About Me</h2>
+          <article className={appStyles.content}>
+            <p>En student inom datateknik på KTH med passion för allt IT-
+              relaterat. Engagemanget sträcker sig bortom klassrummet;
+              systemutveckling är min stora hobby. Mina andra intressen är
+              styrketräning, film, bilar och att umgås med mina vänner. Som
+              fritidsprojekt har jag kodat på Raspberry PI, skapat rörliga
+              hemsidor med JavaScript, och videoredigerat klipp för YouTube.
+              Jag är någon som är ödmjuk och villig att lära sig; som kassör på
+              Filmstaden har jag lärt mig vara kommunikativ och social; som
+              lagermedarbetare på Hydroscand har jag lärt mig vara
+              organiserad och detaljorienterad; som butiksmedarbetare har
+              jag lärt mig kundservice, lagerhantering och effektiv logistik.
+            </p>
+          </article>
+          <aside className={appStyles.aside}>
+            <ul className={appStyles.languages}>
+              <h3>Languages</h3>
+              <li>Swedish</li>
+              <li className={appStyles.skillLvl}>fluent</li>
+              <li>English</li>
+              <li className={appStyles.skillLvl}>fluent</li>
+              <li>Bosnian</li>
+              <li className={appStyles.skillLvl}>native</li>
+            </ul>
+            <ul className={appStyles.attributes}>
+              <h3>Attributes</h3>
+              <li>Self-Learning</li>
+              <li>Adaptability</li>
+              <li>Creativity</li>
+              <li>Team Player</li>
+              <li>Attention to Detail</li>
+            </ul>
+            <ul className={appStyles.hobbies}>
+              <h3>Hobbies</h3>
+              <li>Gym</li>
+              <li>Motorsport</li>
+              <li>Fotball</li>
+            </ul>
+            <ul className={appStyles.socials}>
+              <h3>Socials</h3>
+              <li>LinkedIn</li>
+              <li>Github</li>
+              <li>Discord</li>
+            </ul>
+          </aside>
+        </section>
+        <section id="career" className={appStyles.career}>
+          <section className={appStyles.experties}>
+            <h2>Experties</h2>
+          </section>
+          <section className={appStyles.projects}>
+            <h2>Projects</h2>
+          </section>
+          <section className={appStyles.github}>
+            <h2>GitHub</h2>
+          </section>
+        </section>
+      </main>
+      <footer>
+        <span>Copyright © <time dateTime="2024">2024</time> Tarik Bratic</span>
+      </footer>
+    </>
   )
 }
 
-export default IndexPage
-
-export const Head = () => <title>Home Page</title>
+export default App
